a0e0ccf81d5a77ec7370b87b3e63b62dd10549e7
Mailosz0812 <89217@student.pb.edu.pl>
2026-03-26 18:27:16 +0100

    feat/fix: add user response class to login response

--- CRMRest/src/main/java/org/mailosz/crmrest/crmuser/auth/AuthenticationService.java
+++ CRMRest/src/main/java/org/mailosz/crmrest/crmuser/auth/AuthenticationService.java
@@ -1,5 +1,8 @@
 package org.mailosz.crmrest.crmuser.auth;
 
+import org.mailosz.crmrest.crmuser.CrmUserEntity;
+import org.mailosz.crmrest.crmuser.response.UserResponse;
+import org.mailosz.crmrest.helpers.Mapper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -11,21 +14,25 @@ public class AuthenticationService {
     private final AuthenticationManager manager;
     private final CrmUserDetailsService detailsService;
     private final JWTUtils utils;
+    private final Mapper<CrmUserEntity, UserResponse> userMapper;
 
     @Value("${jwt.expiresIn}")
     private Duration expiresIn;
 
-    public AuthenticationService(AuthenticationManager manager, CrmUserDetailsService detailsService, JWTUtils utils) {
+    public AuthenticationService(AuthenticationManager manager, CrmUserDetailsService detailsService,
+                                 JWTUtils utils, Mapper<CrmUserEntity, UserResponse> userMapper) {
         this.manager = manager;
         this.detailsService = detailsService;
         this.utils = utils;
+        this.userMapper = userMapper;
     }
 
     public LoginResponse login(LoginRequest req){
         manager.authenticate(new UsernamePasswordAuthenticationToken(req.getMail(),req.getPassword()));
         CrmUserDetails userDetails = detailsService.loadUserByUsername(req.getMail());
+        CrmUserEntity userEntity = userDetails.getCrmUserEntity();
         String token = this.utils.generateToken(userDetails);
-        return new LoginResponse(token,expiresIn.toSeconds());
+        return new LoginResponse(token,expiresIn.toSeconds(),this.userMapper.mapFrom(userEntity));
     }
 
 }
--- CRMRest/src/main/java/org/mailosz/crmrest/crmuser/auth/LoginResponse.java
+++ CRMRest/src/main/java/org/mailosz/crmrest/crmuser/auth/LoginResponse.java
@@ -1,31 +1,30 @@
 package org.mailosz.crmrest.crmuser.auth;
 
 
+import org.mailosz.crmrest.crmuser.response.UserResponse;
+
 public class LoginResponse {
     private String token;
     private Long expiresIn;
+    private UserResponse userInfo;
 
-    public LoginResponse() {
-    }
+    public LoginResponse() {}
 
-    public LoginResponse(String token, Long expiresIn) {
+    public LoginResponse(String token, Long expiresIn, UserResponse userResponse) {
         this.token = token;
         this.expiresIn = expiresIn;
+        this.userInfo = userResponse;
     }
 
     public String getToken() {
         return token;
     }
 
-    public void setToken(String token) {
-        this.token = token;
-    }
-
     public Long getExpiresIn() {
         return expiresIn;
     }
 
-    public void setExpiresIn(Long expiresIn) {
-        this.expiresIn = expiresIn;
+    public UserResponse getUserInfo() {
+        return userInfo;
     }
 }
