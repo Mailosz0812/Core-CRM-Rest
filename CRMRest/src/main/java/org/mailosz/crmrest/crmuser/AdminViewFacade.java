b509bcf3b689d1699e8dec24681c29b857bbe341
Mailosz0812 <89217@student.pb.edu.pl>
2026-06-03 14:02:38 +0200

    feat: Add statistics handling in existing code

--- CRMRest/src/main/java/org/mailosz/crmrest/crmuser/AdminViewFacade.java
+++ CRMRest/src/main/java/org/mailosz/crmrest/crmuser/AdminViewFacade.java
@@ -3,6 +3,7 @@ package org.mailosz.crmrest.crmuser;
 import org.mailosz.crmrest.crmuser.response.AdminDashboardView;
 import org.mailosz.crmrest.sales.SaleService;
 import org.mailosz.crmrest.sales.response.ShortSaleResp;
+import org.mailosz.crmrest.stats.StatsService;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -13,14 +14,16 @@ import java.util.List;
 @Component
 public class AdminViewFacade {
     private final SaleService saleService;
+    private final StatsService statsService;
 
-    public AdminViewFacade(SaleService saleService) {
+    public AdminViewFacade(SaleService saleService, StatsService statsService) {
         this.saleService = saleService;
+        this.statsService = statsService;
     }
 
     public AdminDashboardView getAdminDashboardInfo(){
         Pageable pageReq = PageRequest.of(0,15, Sort.by("createdAt").descending());
         List<ShortSaleResp> sales = saleService.getAllSales(pageReq,null,null);
-        return new AdminDashboardView(sales);
+        return new AdminDashboardView(sales,statsService.findMonthlyStats());
     }
 }
--- CRMRest/src/main/java/org/mailosz/crmrest/crmuser/response/AdminDashboardView.java
+++ CRMRest/src/main/java/org/mailosz/crmrest/crmuser/response/AdminDashboardView.java
@@ -1,19 +1,26 @@
 package org.mailosz.crmrest.crmuser.response;
 
 import org.mailosz.crmrest.sales.response.ShortSaleResp;
+import org.mailosz.crmrest.stats.response.StatsResponse;
 
 import java.util.List;
 
 public class AdminDashboardView {
     private List<ShortSaleResp> sales;
-//    private Statistics stats;
+    private StatsResponse stats;
 //    private List<Reminder> reminders
 
-    public AdminDashboardView(List<ShortSaleResp> sales) {
+
+    public AdminDashboardView(List<ShortSaleResp> sales, StatsResponse stats) {
         this.sales = sales;
+        this.stats = stats;
     }
 
     public List<ShortSaleResp> getSales() {
         return sales;
+    }
+
+    public StatsResponse getStats() {
+        return stats;
     }
 }
