d7c8842460456f59f83a46a83d755070e04080f9
Mailosz0812 <89217@student.pb.edu.pl>
2026-06-23 15:52:52 +0200

    fix: Reminder Repository double equal sign fix

--- CRMRest/src/main/java/org/mailosz/crmrest/reminder/ReminderRepository.java
+++ CRMRest/src/main/java/org/mailosz/crmrest/reminder/ReminderRepository.java
@@ -14,7 +14,7 @@ import java.util.UUID;
 @Repository
 public interface ReminderRepository extends JpaRepository<ReminderEntity, UUID> {
 
-    @Query("SELECT r FROM ReminderEntity r WHERE r.isCompleted == false AND r.reminderAt <= :reminderAt AND r.user = :user")
+    @Query("SELECT r FROM ReminderEntity r WHERE r.isCompleted = false AND r.reminderAt <= :reminderAt AND r.user = :user")
     List<ReminderEntity> findDailyReminders(@Param("reminderAt") OffsetDateTime reminderAt, @Param("user") CrmUserEntity user);
 
     Optional<ReminderEntity> findReminderEntityById(UUID id);
